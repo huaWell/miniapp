d9a86119ca9c59f629f731ecab29a17f5747415b
MASON-SEA <yangyangmason@outlook.com>
2024-08-01 16:18:03 +0800

    我的添加关注订单

---
+++ src/pages/order/index.vue
@@ -0,0 +1,40 @@
+<style lang="less" scoped>
+@import '~@/styles/color.less';
+</style>
+
+<template>
+<div class="webview-container">
+    <web-view :src="url">
+
+    </web-view>
+</div>
+</template>
+
+<script>
+let random = () => {
+  return parseInt(Math.random() * 10000)
+}
+import qs from 'querystring'
+
+export default {
+    name: 'webView',
+    components:{},
+    data(){
+        return {
+            //url: "https://operation.bugongsoft.com"
+            url: "https://operation.bugongsoft.com/#/order"
+            //url: "https://www.baidu.com"
+        }
+    },
+    mounted(){
+        console.log(this.url)
+        this.url = `https://operation.bugongsoft.com/#/order/?v=${random()}`
+    },
+    onShow(){
+       // this.url = decodeURIComponent(this.$mp.query.url)
+    },
+    methods:{
+
+    }
+}
+</script>
--- src/pages/wode/index.vue
+++ src/pages/wode/index.vue
@@ -27,6 +27,7 @@
       width: 80px;
 
       position: relative;
+
       .avatar {
         display: block;
         height: 100%;
@@ -39,7 +40,8 @@
         bottom: 0;
         right: 0;
       }
-      .icon--check{
+
+      .icon--check {
         position: absolute;
         bottom: 0;
         left: 0;
@@ -64,85 +66,89 @@
 
 }
 
- .qipao_wrap{
-    width: 100vw;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    position: fixed;
-    top: 120px;
-    z-index: 999;
-    opacity: 0.9;
-    padding-right: 16px;
-    box-sizing: border-box;
+.qipao_wrap {
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: fixed;
+  top: 120px;
+  z-index: 999;
+  opacity: 0.9;
+  padding-right: 16px;
+  box-sizing: border-box;
 }
-   .qipao{
-        width: 200px;
-        height: 50px;
-        // border: 2px solid #ff0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        left: 20px;
-        border-radius: 16px;
-        background-color: #00C35A;
-        color: #fff;
-        padding-left: 10px;
-        padding-right: 10px;
-    	}
-        .qipao_none{
-            display: none;
-        }
-    .qipao::after {
-     content: "";
-      position: absolute;
-      bottom: 100%;
-      right: 56px;
-      margin-left: -5px;
-      border-width: 10px;
-      border-style: solid;
-      border-color: transparent transparent #00C35A transparent;
-	  }
 
-    .kefu{
-      position: fixed;
-      bottom: 30px;
-      right: 30px;
-      z-index: 2;
-      border-radius: 50%;
-      background: #07c160;
-      box-shadow: 1px 4px 5px #dbd7d5;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      width: 80px;
-      height: 80px;
-      .kefu-text{
-        font-size: 12px;
-        color: #fff;
-      }
-    }
+.qipao {
+  width: 200px;
+  height: 50px;
+  // border: 2px solid #ff0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 20px;
+  border-radius: 16px;
+  background-color: #00C35A;
+  color: #fff;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.qipao_none {
+  display: none;
+}
+
+.qipao::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  right: 56px;
+  margin-left: -5px;
+  border-width: 10px;
+  border-style: solid;
+  border-color: transparent transparent #00C35A transparent;
+}
+
+.kefu {
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  z-index: 2;
+  border-radius: 50%;
+  background: #07c160;
+  box-shadow: 1px 4px 5px #dbd7d5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+
+  .kefu-text {
+    font-size: 12px;
+    color: #fff;
+  }
+}
 </style>
 <template>
   <div class="wode-container" @click="hideTip">
     <!-- <button @click="onWX">微信相关</button> -->
     <div class="header-wrapper">
       <div class="left">
-        <div class="name">{{name}}</div>
+        <div class="name">{{ name }}</div>
         <div class="desc">重剑无锋，大巧不工</div>
       </div>
       <span class="flex"></span>
       <div class="avatar-block" @click="onEditInfo">
         <img :src="bugong" alt="" class="avatar">
         <div class="icon icon--edit"></div>
-        <div class="icon icon--check" v-if="userInfo.secCheckStatus==1"></div>
+        <div class="icon icon--check" v-if="userInfo.secCheckStatus == 1"></div>
       </div>
     </div>
     <div class="cell-group-wrapper">
       <van-cell-group :border="false">
         <van-cell @click="onGotoMyCollection" custom-class="my-van-cell--large" size="large" title="策略参数" is-link>
-          <div slot="icon" class="icon icon--collection"></div>
+          <div slot="icon" class="icon icon--fresh"></div>
         </van-cell>
 
         <!-- <van-cell @click="onGotoMessage" custom-class="my-van-cell--large" size="large" title="消息中心" :border="false" is-link>
@@ -158,8 +164,8 @@
           <div slot="icon" class="icon icon--ask-buy"></div>
         </van-cell>
 
-        <van-cell @click="onGotoMyFresh" custom-class="my-van-cell--large" size="large" title="代办功能4" is-link>
-          <div slot="icon" class="icon icon--fresh"></div>
+        <van-cell @click="onGotoMyFresh" custom-class="my-van-cell--large" size="large" title="关注订单" is-link>
+          <div slot="icon" class="icon icon--collection"></div>
         </van-cell>
 
         <!-- <van-cell custom-class="my-van-cell--large" size="large" title="设置" is-link :border="false">
@@ -168,8 +174,8 @@
       </van-cell-group>
     </div>
 
-    <div :class="[showTip?'qipao_wrap':'qipao_wrap qipao_none']" class="qipao_wrap" :show="false">
-            <div class="qipao" :show="showTip">{{tip}}</div>
+    <div :class="[showTip ? 'qipao_wrap' : 'qipao_wrap qipao_none']" class="qipao_wrap" :show="false">
+      <div class="qipao" :show="showTip">{{ tip }}</div>
     </div>
 
     <!-- <div>
@@ -191,50 +197,50 @@ export default {
   data() {
     return {
       name: '不工',
-      avatar:'',
+      avatar: '',
       days: 0,
-      showTip:false,
+      showTip: false,
       tip: '此处可修改你的头像和名字',
-      userInfo:{},
-      bugong:bugong
+      userInfo: {},
+      bugong: bugong
     }
   },
   mounted() {
 
 
   },
-  async onShow () {
+  async onShow() {
   },
   methods: {
-    hideTip(){
+    hideTip() {
       this.showTip = false;
       this.$store.commit('USER_IS_FIRST_LOGIN', false);
     },
-    onEditInfo () {
-      let param = {name: this.name, avatar: this.avatar};
+    onEditInfo() {
+      let param = { name: this.name, avatar: this.avatar };
 
       let param_str = encodeURIComponent(JSON.stringify(param));
 
     },
-    onGotoMessage () {
+    onGotoMessage() {
 
     },
-    onGotoMyPublish () {
+    onGotoMyPublish() {
 
     },
-    onGotoMyCollection () {
+    onGotoMyCollection() {
 
     },
-    onWX () {
+    onWX() {
 
     },
-    onGotoMyAskBuy(){
+    onGotoMyAskBuy() {
 
     },
-     onGotoMyFresh(){
-
+    onGotoMyFresh() {
+      this.$push('/pages/order/main');
     },
-    async onGotoObject(){
+    async onGotoObject() {
       //let {latitude, longitude} = await getLocation()
     }
   }
